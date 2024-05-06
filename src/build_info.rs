967bd0b931c269d862178316c56d20b0ad962035
Alex Haslehurst <alex.haslehurst@gmail.com>
2024-05-06 23:02:55 +0100

    remove build_info

--- src/build_info.rs
+++ src/build_info.rs
@@ -1,8 +1,3 @@
-pub const BUILD_INFO: &str = build_info::format!(
-    "{} v{} by {}",
-    $.crate_info.name,
-    $.crate_info.version,
-    $.crate_info.authors,
-);
+pub const BUILD_INFO: &str = "rustris v1.2.0 by Alex Haslehurst";
 
-pub const APP_NAME: &str = build_info::format!("{}", $.crate_info.name);
+pub const APP_NAME: &str = "rustris";
